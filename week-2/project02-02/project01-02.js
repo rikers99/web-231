cbfd6d916dfb252425f32fcd55d389aa1c813b95
Steve Culmer <culmersteve11@gmail.com>
2024-08-16 16:19:44 -0400

    update web231

---
+++ week-2/project02-02/project01-02.js
@@ -0,0 +1,12 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Steve Culmer
+    Date: 16 Aug 2024
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+service1Name = "Basic", service2Name = "Express", service3Name = "extreme", service4Name = "Ultimate", service1Speed = "0 Mbps", service2Speed = "100 Mbps", service3Speed = "500Mbps", and service4Speed = "1Gig".
