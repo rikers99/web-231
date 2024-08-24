bef5a11d7af119f6aaeedddffcad77d20c8dd379
Steve Culmer <culmersteve11@gmail.com>
2024-08-24 14:54:08 -0400

    update js02.js

--- week-2/chapter/js02.js
+++ week-2/chapter/js02.js
@@ -34,11 +34,23 @@ function getEstimate() {
     let photographers = document.getElementById("photoNum").value;
     let hours = document.getElementById("photoHrs").value;
     let distance = document.getElementById("photoDist").value;
+    let buyBook = document.getElementById("makeBook").checked;
+    let buyRights = document.getElementById("photoRights").checked;
 
     // add the cost of photographers for the hours covered
     totalCost += photographers * hours * EMP_COST;
 
     // Add the cost of distance per photographer per mile
     totalCost += photographers * distance * TRAVEL_COST;
+
+    //Add the cost of the book if purchased
+    totalCost += buyBook ? BOOK_COST : 0;
+
+    //Add the cost of photo rights if purchased
+    totalCost += buyRights ? REPRO_COST : 0;
 }
+
+
+
+
 
