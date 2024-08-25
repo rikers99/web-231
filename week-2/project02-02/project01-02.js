57f275924898ab416c6b2886bad04dfb8ae5f246
Steve Culmer <culmersteve11@gmail.com>
2024-08-25 07:07:44 -0400

    update pro

--- week-2/project02-02/project01-02.js
+++ week-2/project02-02/project01-02.js
@@ -8,5 +8,3 @@
     Filename: project01-02.js
 */
 
-//define variables for service name and service speed
-service1Name = "Basic", service2Name = "Express", service3Name = "extreme", service4Name = "Ultimate", service1Speed = "0 Mbps", service2Speed = "100 Mbps", service3Speed = "500Mbps", and service4Speed = "1Gig".
---
+++ week-2/project02-02/project02-02.js
@@ -0,0 +1,16 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-02
+
+      Application to test for completed form
+      Author: 
+      Date:   
+
+      Filename: project02-02.js
+ */
+ 
+// declare global constants for the application
+function verifyForm():
+      let Name = "name";
+      let Email = "email";
+      let phone = "Phone";
