b8c0fb72a17c84fe55a449ea6877d3816b4607cc
Steve Culmer <culmersteve11@gmail.com>
2024-08-25 08:07:20 -0400

    update project02-02

--- week-2/project02-02/project02-02.js
+++ week-2/project02-02/project02-02.js
@@ -3,14 +3,27 @@
       Project 02-02
 
       Application to test for completed form
-      Author: 
-      Date:   
+      Author: Steve Culmer
+      Date:   25 Aug 24
 
       Filename: project02-02.js
  */
  
 // declare global constants for the application
-function verifyForm():
-      let Name = "name";
-      let Email = "email";
-      let phone = "Phone";
+function verifyForm() {
+      let name = document.getElementById("nameInput").value;
+      let email = document.getElementById("emailInput").value;
+      let phone = document.getElementById("phoneInput").value;
+
+      name  = (nameInput === "Name" && name === "");
+      email = (emailInput === "Email" && email === "");
+      phone = (phoneInput === "Phone" && phone === "");
+      document.getElementById("Submit").innerHTML = "$" + "Thank You!";
+
+      name = (nameInput === "" && name === "");
+      email = (emailInput === "" && email === "");
+      phone = (phoneInput === "" && phone === "");
+      document.getElementById("Submit").innerHTML = "$" + "Please fill in all fields";
+
+      getSubmit();
+}
