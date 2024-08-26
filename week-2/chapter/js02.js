0caa2e001c475e8346920200dc518afedf9a99a6
Steve Culmer <culmersteve11@gmail.com>
2024-08-25 21:40:22 -0400

    update

---
+++ week-2/chapter/js02.js
@@ -0,0 +1,21 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Chapter case
+
+      Fan Trick Fine Art Photography
+      Variables and functions
+      Author: 
+      Date:   
+
+      Filename: js02.js
+ */
+
+// set the form's default values
+function setupForm() {
+    document.getElementById("photoNum").value = 1;
+    document.getElementById("photoHrs").value = 2;
+    document.getElementById("makeBook").checked = false;
+    Document.getElementById("photoRights").checked = false;
+    document.getElementById("photoDist").value = 0;
+}
+
