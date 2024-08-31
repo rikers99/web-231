4b984ee9b45787c9f245bf409db0e1aa592f3758
Steve Culmer <culmersteve11@gmail.com>
2024-08-31 12:04:30 -0400

    update project03-01

--- week-4/project03-01/project03-01.js
+++ week-4/project03-01/project03-01.js
@@ -9,6 +9,26 @@
       Filename: project03-01.js
 */
 
+//reference the collection of menu items
+let menuItem = document.getElementsByTagName("id");
+
+
+
+     //Function to write menu items
+     function menuItems() {
+        let i = 0; //initial counter value
+        
+        window.addEventListener("load", menuItems);
+
+        //write each of the menu items
+        while (i < 5) {
+            id[i].innerHTML = menuItems[i];
+
+            //increase the counter by 1
+            i++;
+        }
+    }
+
 
 
 
