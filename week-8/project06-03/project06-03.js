965c31628dd877884bb8c5cceaa605d838f02774
Steve Culmer <culmersteve11@gmail.com>
2024-10-06 09:09:17 -0400

    update all

---
+++ week-8/project06-03/project06-03.js
@@ -0,0 +1,16 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 6
+      Project 06-03
+
+      Script to complete a form containing billing and shipping address information
+      Author: steve culmer
+      Date:   5 oct 24
+
+      Filename: project06-03.js
+*/
+
+window.addEventListener("load", function() {
+  let useShip = document.forms.useShip;
+
+});
